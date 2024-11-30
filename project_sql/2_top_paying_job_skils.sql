dc7a9bab49104968bd196419f6fd87bbddd7a125
audreycj <68633976+audreycj@users.noreply.github.com>
2024-11-30 14:51:00 +0800

    refactor: format salary as currency

--- project_sql/2_top_paying_job_skils.sql
+++ project_sql/2_top_paying_job_skils.sql
@@ -12,7 +12,7 @@ WITH top_paying_jobs AS (
     SELECT
         job_id,
         job_title,
-        salary_year_avg,
+        TO_CHAR(ROUND(salary_year_avg, 0), 'FM$999,999,999') AS avg_yearly_salary,
         name AS company_name
     FROM
         job_postings_fact
@@ -35,7 +35,7 @@ FROM
 INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 ORDER BY
-    salary_year_avg DESC;
+    avg_yearly_salary DESC;
 
 /*
 
